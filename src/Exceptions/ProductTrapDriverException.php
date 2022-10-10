b75297177c297f80f3569075fa7801ae3329bd12
Owen Voke <development@voke.dev>
2022-10-10 18:12:20 +0100

    feat: add exception interface

--- src/Exceptions/ProductTrapDriverException.php
+++ src/Exceptions/ProductTrapDriverException.php
@@ -4,9 +4,10 @@ namespace ProductTrap\Exceptions;
 
 use Exception;
 use ProductTrap\Contracts\Driver;
+use ProductTrap\Contracts\ProductTrapException;
 use Throwable;
 
-abstract class ProductTrapDriverException extends Exception
+abstract class ProductTrapDriverException extends Exception implements ProductTrapException
 {
     public function __construct(public readonly Driver $driver, string $message = '', int $code = 0, ?Throwable $previous = null)
     {
