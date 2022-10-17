2d820783804c45fc20b190d2545aacae8c308a90
Bradie <44430471+bradietilley@users.noreply.github.com>
2022-10-17 18:48:12 +0800

    feat: add helpers and TYPE constants to Query DTO to help with querying

--- src/DTOs/Query.php
+++ src/DTOs/Query.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace ProductTrap\DTOs;
 
 use Spatie\DataTransferObject\Attributes\Strict;
+use Stringable;
 
 #[Strict]
-class Query extends DataTransferObject
+class Query extends DataTransferObject implements Stringable
 {
+    public const TYPE_KEYWORD = 'keyword';
+    public const TYPE_CATEGORY = 'category';
+    public const TYPE_BRAND = 'brand';
+
     public ?string $keywords = null;
 
     public ?Category $category = null;
@@ -36,6 +41,11 @@ class Query extends DataTransferObject
         );
     }
 
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
     /**
      * If a driver does not support Brand or Category pages, calling toString
      * will cast this object to a string and will return the brand, category
@@ -48,5 +58,23 @@ class Query extends DataTransferObject
             $this->category?->name,
             $this->brand?->name,
         ])));
+    }
+
+    public function type(): string
+    {
+        if ($this->category !== null) {
+            return static::TYPE_CATEGORY;
+        }
+
+        if ($this->brand !== null) {
+            return static::TYPE_BRAND;
+
+        }
+        return static::TYPE_KEYWORD;
+    }
+
+    public function cacheKey(): string
+    {
+        return 'producttrap.query.' . hash('sha256', json_encode($this->toArray()));
     }
 }
