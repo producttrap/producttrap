57161652f4b082b4290c29c400a52cc6877db552
Owen Voke <development@voke.dev>
2023-04-27 14:30:22 +0100

    chore: fix facade auto-completion to be static

--- src/Facades/ProductTrap.php
+++ src/Facades/ProductTrap.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace ProductTrap\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use ProductTrap\Contracts\Driver;
+use ProductTrap\DTOs\Product;
 
-/** @mixin \ProductTrap\ProductTrap */
+/**
+ * @method static Driver driver(string|null $driver = null)
+ * @method static Product find(string $identifier, array $parameters = [])
+ */
 class ProductTrap extends Facade
 {
     protected static function getFacadeAccessor(): string
